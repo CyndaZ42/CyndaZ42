4ba095a8f7414da509dd84715c83f1b4fea0516b
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-12-21 12:20:27 -0800

    move navbar

--- src/index.js
+++ src/index.js
@@ -4,24 +4,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
 //Functions
 
-function knownLanguages(languages) {
-  let styles = "language-icon";
-  let size = "60";
-  languages.forEach(element => {
-    $("#knownLanguages").append(`<img src="${element.src}" alt="${element.name}" class="${styles}" width="${size}" height="${size}"/>`);
-  });
-}
-
-function currentlyLearning(languages) {
-  let styles = "language-icon";
-  let size = "60";
-  languages.forEach(element => {
-    $("#currentlyLearning").append(`<img src="${element.src}" alt="${element.name}" class="${styles}" width="${size}" height="${size}"/>`);
-  });
-}
-
+  //Global
+  
 function makeHeader(pages) {
   let styles = "menu-item";
   $(".menu-bar").append(`<p class="menu">`);
@@ -36,9 +23,36 @@ function makeHeader(pages) {
 }
   setTimeout(loading, 4000); */
 
+  //Homepage
+function knownLanguages(languages) {
+  let styles = "language-icon";
+  let size = "70";
+  languages.forEach(element => {
+    $("#knownLanguages").append(`<img src="${element.src}" alt="${element.name}" class="${styles}" width="${size}" height="${size}"/>`);
+  });
+}
+
+function currentlyLearning(languages) {
+  let styles = "language-icon";
+  let size = "70";
+  languages.forEach(element => {
+    $("#currentlyLearning").append(`<img src="${element.src}" alt="${element.name}" class="${styles}" width="${size}" height="${size}"/>`);
+  });
+}
+
+
+  //Resume
+function makeSkills(skills) {
+  let styles = "";
+  skills.forEach(element => {
+    $(".skills").append(`<p class="${styles}">${element.name}</p>`);
+  });
+}
+
 //Document Ready
 $(document).ready(function(){
   makeHeader(data.pages);
   knownLanguages(data.knownLanguages);
   currentlyLearning(data.currentlyLearning);
+  makeSkills(data.skills);
 });
